6e4e781339818def2b9d0eab46da95c925527fb0
TheHabibi <celebialieren@gmail.com>
2023-07-31 23:51:55 +0300

    Update EmployeeService.java

--- src/main/java/com/ali/cyangatespringcase/service/EmployeeService.java
+++ src/main/java/com/ali/cyangatespringcase/service/EmployeeService.java
@@ -12,6 +12,8 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Base64;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Service
 public class EmployeeService {
@@ -30,18 +32,20 @@ public class EmployeeService {
         List<Employee> employees = new ArrayList<>();
         try (Workbook workbook = WorkbookFactory.create(new ByteArrayInputStream(decodedBytes))) {
             Sheet sheet = workbook.getSheetAt(0);
-            for (int i = 1; i < sheet.getLastRowNum() + 1; i++) { 
-                Row row = sheet.getRow(i);
-                Employee employee = new Employee();
-                employee.setName(row.getCell(0).getStringCellValue());
-                employee.setSurname(row.getCell(1).getStringCellValue());
-                employee.setAge((int) row.getCell(2).getNumericCellValue());
-                employee.setSalary((int) row.getCell(3).getNumericCellValue());
-                employee.setWorkYears((int) row.getCell(4).getNumericCellValue());
-                employee.setTitle(row.getCell(5).getStringCellValue());
-                employees.add(employee);
-                System.out.println(employee);
-            }
+            employees = IntStream.range(1, sheet.getLastRowNum() + 1)
+                    .mapToObj(sheet::getRow)
+                    .map(row -> {
+                        Employee employee = new Employee();
+                        employee.setName(row.getCell(0).getStringCellValue());
+                        employee.setSurname(row.getCell(1).getStringCellValue());
+                        employee.setAge((int) row.getCell(2).getNumericCellValue());
+                        employee.setSalary((int) row.getCell(3).getNumericCellValue());
+                        employee.setWorkYears((int) row.getCell(4).getNumericCellValue());
+                        employee.setTitle(row.getCell(5).getStringCellValue());
+                        System.out.println(employee);
+                        return employee;
+                    })
+                    .collect(Collectors.toList());
         } catch (IOException e) {
             throw new IOException("Error parsing spreadsheet data", e);
         }
